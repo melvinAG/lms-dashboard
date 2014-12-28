8a37068d58ec61076e1d829e9bb4c333b8facbc7
melvinAG <melvin@avant-garded.com>
2014-12-28 23:26:40 +0000

    Modeled users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141228192143) do
+ActiveRecord::Schema.define(version: 20141228231527) do
 
   create_table "enrollments", force: true do |t|
     t.integer  "user_id"
@@ -30,8 +30,11 @@ ActiveRecord::Schema.define(version: 20141228192143) do
   create_table "users", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
