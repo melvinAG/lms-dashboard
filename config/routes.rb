9759882bb1913a45c7ff9f69defdc982aff2c9fe
melvinAG <melvin@avant-garded.com>
2014-12-29 22:22:39 +0000

    Updated unicorn files

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
 
   resources :users
 
-config.force_ssl = true
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
